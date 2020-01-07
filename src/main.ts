53d582c9e888ba666466568af6169429cd0277c8
Shikhar <shikhar@bitqit.com>
2020-01-07 19:00:19 +0530

    css changes.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import Vue from 'vue'
-import App from './App.vue'
+import './plugins/fontawesome'
+import JobListing from './components/JobListing.vue';
+import DepartmentBox from './components/DepartmentBox.vue';
+
+
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+
+library.add(faFacebookF)
+library.add(faTwitter)
+library.add(faLinkedinIn)
+
+
+
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+
+
+  new Vue({
+    render: h => h(JobListing),
+  }).$mount('#job-listing')
+
+
+  new Vue({
+    render: h => h(DepartmentBox),
+  }).$mount('#department-box')
+
+
